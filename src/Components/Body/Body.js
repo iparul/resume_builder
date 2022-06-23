c832e7f6000741cbcba9fd0c8b0f46484e0062fd
parul.gupta <parul@nssplindia.com>
2022-06-23 12:53:39 +0530

    first commit

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactToPrint from "react-to-print";
+import styles from "./Body.module.css";
+import { ArrowDown } from "react-feather";
+import Editor from "../Editor/Editor";
+import Resume from "../Resume/Resume";
+import Resume1 from "../Resume1/Resume1";
+
+function Body() {
+  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
+  const sections = {
+    basicInfo: "Basic Info",
+    workExp: "Work Experience",
+    project: "Projects",
+    education: "Education",
+    achievements: "Achievements",
+    // skills: "Skills",
+    summary: "Summary",
+    other: "Other",
+  };
+  const resumeRef = useRef();
+  const [activeColor, setActiveColor] = useState(colors[0]);
+  const [resumeInformation, setResumeInformation] = useState({
+    [sections.basicInfo]: {
+      id: sections.basicInfo,
+      sectionTitle: sections.basicInfo,
+      detail: {},
+    },
+    [sections.workExp]: {
+      id: sections.workExp,
+      sectionTitle: sections.workExp,
+      details: [],
+    },
+    [sections.project]: {
+      id: sections.project,
+      sectionTitle: sections.project,
+      details: [],
+    },
+    [sections.education]: {
+      id: sections.education,
+      sectionTitle: sections.education,
+      details: [],
+    },
+    [sections.achievements]: {
+      id: sections.achievements,
+      sectionTitle: sections.achievements,
+      points: [],
+    },
+    // [sections.skills]: {
+    //   id: sections.skills,
+    //   sectionTitle: sections.skills,
+    //   points: [],
+    // },
+    [sections.summary]: {
+      id: sections.summary,
+      sectionTitle: sections.summary,
+      detail: "",
+    },
+    [sections.other]: {
+      id: sections.other,
+      sectionTitle: sections.other,
+      detail: "",
+    },
+  });
+  useEffect(() => {
+    console.log(resumeInformation);
+  }, [resumeInformation]);
+  return (
+    <div className={styles.container}>
+      <p className={styles.heading}>Resume Builder</p>
+      <div className={styles.toolbar}>
+        <div className={styles.colors}>
+          {colors.map((item) => (
+            <span
+              key={item}
+              onClick={() => setActiveColor(item)}
+              style={{ backgroundColor: item }}
+              className={`${styles.color} ${
+                activeColor === item ? styles.active : ""
+              }`}
+            />
+          ))}
+        </div>
+        <ReactToPrint
+          trigger={() => {
+            return (
+              <button>
+                Download <ArrowDown />
+              </button>
+            );
+          }}
+          content={() => resumeRef.current}
+        />
+      </div>
+      <div className={styles.main}>
+        <Editor
+          sections={sections}
+          information={resumeInformation}
+          setInformation={setResumeInformation}
+        />
+        {/* <div className={styles.set}> */}
+        <Resume
+          ref={resumeRef}
+          sections={sections}
+          information={resumeInformation}
+          activeColor={activeColor}
+        />
+        {/* <Resume1
+            ref={resumeRef}
+            sections={sections}
+            information={resumeInformation}
+            activeColor={activeColor}
+          /> */}
+        {/* </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default Body;
